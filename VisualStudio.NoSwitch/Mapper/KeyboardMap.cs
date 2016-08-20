975d3fca2e195f0a3c405be5fcd0d498f2e2342a
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-08-21 03:19:58 +0700

    Added simple replace without release

---
+++ VisualStudio.NoSwitch/Mapper/KeyboardMap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VisualStudio.NoSwitch.Mapper
+{
+    class KeyboardMap
+    {
+        private static readonly String ThaiKeys =
+            "ๅ/-ภถุึคตจขชๆไำพะัีรนยบลฟหกดเ้่าสวงผปแอิืทมใฝ+๑๒๓๔ู฿๕๖๗๘๙๐\"ฎฑธํ๊ณฯญฐ,ฤฆฏโฌ็๋ษศซ.()ฉฮฺ์?ฒฬฦ";
+        private static readonly String EnglishKey =
+            "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?";
+
+        private static Dictionary<char, char> CreateDict()
+        {
+            var dict = new Dictionary<char, char>();
+            EnglishKey.ToCharArray().Select((Element, Index) => new { Index, Element }).ToList().ForEach(x =>
+                    {
+                        dict[x.Element] = ThaiKeys[x.Index];
+                    });
+
+            return dict; ;
+        }
+
+        public static Dictionary<char, char> Maps { get; } = CreateDict();
+        public static char GetThaiChar(char englishChar)
+        {
+            if (Maps.ContainsKey(englishChar))
+            {
+                return Maps[englishChar];
+            }
+            return englishChar;
+        }
+    }
+}
--- VisualStudio.NoSwitch/Tag/NoSwitchTagger.cs
+++ VisualStudio.NoSwitch/Tag/NoSwitchTagger.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Operations;
 using System.Linq;
+using VisualStudio.NoSwitch.Mapper;
 
 namespace VisualStudio.NoSwitch.Tag
 {
+    class State
+    {
+        public bool ActivateThai { set; get; }
+        public int StartPosition { set; get; }
+    }
+
     class NoSwitchTagger : ITagger<NoSwitchTag>
     {
+        private State _state = new State();
+
         public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
 
         private ITextSearchService2 _searchService;
@@ -21,33 +30,101 @@ namespace VisualStudio.NoSwitch.Tag
             _searchService = search;
         }
 
+        private void Release(SnapshotSpan span)
+        {
+            //ReplaceWith(span, '~', "");
+            _state.ActivateThai = false;
+            _state.StartPosition = 0;
+        }
+
+        private void Start(int location)
+        {
+            _state.ActivateThai = true;
+            _state.StartPosition = location;
+        }
+
+        private void ReplaceWith(SnapshotSpan span, char replace, string with)
+        {
+            var edit = _buffer.CreateEdit();
+            edit.Replace(_state.StartPosition, replace, with);
+            edit.Apply();
+        }
+
+        private void Replace(SnapshotSpan span, string replaceWith)
+        {
+            var edit = _buffer.CreateEdit();
+            edit.Replace(span, replaceWith);
+            edit.Apply();
+        }
+
+
         public IEnumerable<ITagSpan<NoSwitchTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            try
+            {
+                return TryGetTags(spans);
+
+            }
+            catch (Exception ex)
+            {
+                return Enumerable.Empty<ITagSpan<NoSwitchTag>>();
+            }
+        }
+
+        public IEnumerable<ITagSpan<NoSwitchTag>> TryGetTags(NormalizedSnapshotSpanCollection spans)
         {
             foreach (SnapshotSpan currentSpan in spans)
             {
                 var containingLine = currentSpan.Start.GetContainingLine();
                 var location = containingLine.Start.Position;
                 var line = containingLine.GetText();
-                var tokens = line.Split(' ');
 
-                foreach (string token in tokens)
+                var start = line.IndexOf('~');
+                if(start != -1)
                 {
-                    if (token == "%%")
+                    _state.StartPosition = start + location;
+                    _state.ActivateThai = true;
+                }
+
+                foreach (var token in line.ToArray())
+                {
+                    var activateThai = _state.ActivateThai;
+                    var startPosition = _state.StartPosition;
+
+                    if (token == '~')
                     {
-                        var index = line.IndexOf(token);
-                        var tokenSpan = new SnapshotSpan(currentSpan.Snapshot, new Span(location, token.Length));
-
-                        //var rs = _searchService.FindAllForReplace(currentSpan, "%", "$", FindOptions.MatchCase);
-                        var edit = _buffer.CreateEdit();
-                        edit.Replace(tokenSpan, "$");
-                        edit.Apply();
-
-                        var tag = new NoSwitchTag(NoSwitchTaggerTypes.Start);
-                        var newSpan = new TagSpan<NoSwitchTag>(tokenSpan, tag);
-                        yield return newSpan;
+                        if (activateThai)
+                        {
+                            if (location > startPosition)
+                            {
+                                //var span = new SnapshotSpan(currentSpan.Snapshot, new Span(startPosition, location - startPosition));
+                                //Release(span);
+                            }
+                        }
+                        else
+                        {
+                            //Start(location);
+                        }
                     }
+                    else
+                    {
+                        if (activateThai && location > startPosition)
+                        {
+                            var thai = KeyboardMap.GetThaiChar(token);
 
-                    location += token.Length + 1;
+                            if (thai != token)
+                            {
+                                var tokenSpan = new SnapshotSpan(currentSpan.Snapshot, new Span(location, 1));
+                                Replace(tokenSpan, thai.ToString());
+
+                            }
+                            var startSpan = new SnapshotSpan(currentSpan.Snapshot, new Span(startPosition, location));
+                            var tag = new NoSwitchTag(NoSwitchTaggerTypes.Start);
+                            var newSpan = new TagSpan<NoSwitchTag>(startSpan, tag);
+                            yield return newSpan;
+                        }
+                    }
+                    location++;
                 }
             }
         }
